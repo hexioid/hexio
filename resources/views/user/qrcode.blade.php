60233b27dc30a052f8bd63d517775275258da8ad
Abi F <abi@digitalgoal.id>
2022-03-24 23:55:56 +0700

    Update qrcode.blade.php

--- resources/views/user/qrcode.blade.php
+++ resources/views/user/qrcode.blade.php
@@ -1,17 +1,18 @@
 @extends("user.user_home_template")
 
 @section("content")
-    <div class="row mt-4 col-12">
+    <div class="row mt-4">
         <div class="col-lg-1 col-md-0 col-sm-0 col-0"></div>
         <div class="shadow p-3 mb-5 bg-white rounded col-lg-10 col-md-12 col-sm-12 col-12">
             <div class="card-body">
                 <div class="form-group">
                     <input type="text" name="username" readonly class="form-control" value="{{env('APP_URL').'/'.$data->username}}" placeholder="username">
                 </div>
+                <br>
                 <div class="text-center">
                     <img class="img-fluid"  src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(400)->generate(env('APP_URL').'/'.$data->username)) !!} ">
                 </div>
-
+                <br>
                 <a href="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(400)->generate(env('APP_URL').'/'.$data->username)) !!} " class="btn btn-dark btn-md float-right" download>DOWNLOAD</a>
             </div>
         </div>
