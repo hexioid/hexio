731f4d712705a7f760256b274128a49d14693786
Abi F <abi@digitalgoal.id>
2022-04-12 19:19:48 +0700

    update + di phone vcard

--- resources/views/user/vcard.blade.php
+++ resources/views/user/vcard.blade.php
@@ -17,15 +17,15 @@
                     </div>
                     <div class="form-group">
                         <label for="telephone" class="form-label">Phone</label>
-                        <input type="number" value="{{old('phone_1', $data->phone_1 ?? '')}}" name="phone_1" class="form-control" placeholder="Telephone">
+                        <input type="text" value="{{old('phone_1', $data->phone_1 ?? '')}}" name="phone_1" class="form-control" placeholder="Telephone">
                     </div>
                     <div class="form-group">
                         <label for="telephone" class="form-label">Phone</label>
-                        <input type="number" value="{{old('phone_2', $data->phone_2 ?? '')}}" name="phone_2" class="form-control" placeholder="Telephone">
+                        <input type="text" value="{{old('phone_2', $data->phone_2 ?? '')}}" name="phone_2" class="form-control" placeholder="Telephone">
                     </div>
                     <div class="form-group">
                         <label for="telephone" class="form-label">Phone</label>
-                        <input type="number" value="{{old('phone_3', $data->phone_3 ?? '')}}" name="phone_3" class="form-control" placeholder="Telephone">
+                        <input type="text" value="{{old('phone_3', $data->phone_3 ?? '')}}" name="phone_3" class="form-control" placeholder="Telephone">
                     </div>
                     <div class="form-group">
                         <label for="address" class="form-label">Address</label>
